16df17806a36a42164ad1f96454ed21e71f5f6b7
feay1234 <feay1234@salisbury.dcs.gla.ac.uk>
2014-06-23 17:03:34 +0100

    add statusbar notification plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -34,13 +34,29 @@ module.exports = [
         "clobbers": [
             "navigator.geolocation"
         ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.phonegap.plugins.facebookconnect": "0.5.1",
-    "org.apache.cordova.geolocation": "0.3.9-dev"
+    "org.apache.cordova.geolocation": "0.3.9-dev",
+    "de.appplant.cordova.plugin.local-notification": "0.7.4",
+    "org.apache.cordova.device": "0.2.11-dev"
 }
 // BOTTOM OF METADATA
 });
